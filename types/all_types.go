cb7c0f0b83d4709d036168a3b1cca60213331e60
cozyCodr <officialcre8tor@gmail.com>
2025-01-31 23:36:45 +0200

    hot fix

--- types/all_types.go
+++ types/all_types.go
@@ -2,6 +2,6 @@ package types
 
 type Response struct {
 	Email           string `json:"email"`
-	CurrentDateTime string `json:"current_date_time"`
+	CurrentDateTime string `json:"current_datetime"`
 	GithubUrl       string `json:"github_url"`
 }
